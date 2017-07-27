8f953f744c1936e005d5273534dddf00ac3b024f
Vanamode <30317969+Vanamode@users.noreply.github.com>
2017-07-27 15:47:39 +0100

    Initial Commit

---
+++ app/main.ts
@@ -0,0 +1,9 @@
+import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
+
+import { AppModule } from "./app.module";
+
+const platform = platformBrowserDynamic();
+platform.bootstrapModule(AppModule);
+
+
+https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js
